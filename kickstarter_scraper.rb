95c3b8b28810a864c516dba9f0b44e86cfc69f7e
Milos Popovic <milos_popovic@outlook.com>
2020-08-04 19:12:48 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,11 +1,12 @@
 require 'nokogiri'
 require 'pry'
+html = File.read('fixtures/kickstarter.html')
+
+kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
-  html = File.read('fixtures/kickstarter.html')
 
-  kickstarter = Nokogiri::HTML(html)
 
   projects = {}
 
